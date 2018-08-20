819ab864776e511ca7f1aee72f24a9a05941b4f2
Marcin Mik <piptrik@poczta.fm>
2018-08-20 18:57:16 +0200

    Quality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import inside from 'point-in-polygon'
-import * as boundaries from './boundaries-poland'
+import inside from 'point-in-polygon';
+import * as boundaries from './boundaries-poland';
 
-const polygon = boundaries.coordinates
+const polygon = boundaries.coordinates;
 
 function isInPoland(lat, lng) {
   if (Number.isNaN(lat)) {
-    throw "Latitude is not a number"
+    throw new Error('Latitude is not a number');
   }
   if (Number.isNaN(lng)) {
-    throw "Longitude is not a number"
+    throw new Error('Longitude is not a number');
   }
-  return inside([lat, lng], polygon)
+  return inside([lat, lng], polygon);
 }
 
 export default isInPoland;
